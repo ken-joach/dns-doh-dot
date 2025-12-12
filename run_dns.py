bc3c6f72367fcd930baa7d4860142424bf262449
Kendra Joachim <joachimk@duq.edu>
2025-12-12 04:05:32 -0500

    Final Code; added relevant graphs and the .csv file

--- run_dns.py
+++ run_dns.py
@@ -27,12 +27,13 @@ QUERY_LIST = {
     "microsoft.com", "netflix.com", "wikipedia.org", "mozilla.org", "github.com",
     "stackoverflow.com", "reddit.com", "nytimes.com", "cnn.com", "bbc.co.uk",
     "mit.edu", "quora.com", "yahoo.com", "harvard.edu", "imgur.com",
-    "ox.ac.uk", "nhk.or.jp", "theguardian.com", "naver.com", "cbc.ca"
-    "cam.ac.uk", "utoronto.ca", "oracle.com", "edf.fr", "sony.jp",
+    "ox.ac.uk", "bing.com", "theguardian.com", "naver.com", "youtube.com",
+    "cam.ac.uk", "utoronto.ca", "oracle.com", "edf.fr", "sony.jp", 
     "kakao.com", "abc.net.au", "auckland.ac.nz", "espn.com", "cisco.com",
     "openai.com", "twitch.tv", "ubuntu.com", "zoom.us", "salesforce.com",
-    "python.org", "dropbox.com", "kubernetes.io", "nodejs.org", "golang.org"
+    "python.org", "dropbox.com", "kubernetes.io", "nodejs.org", "golang.org", "instagram.com"
 }
+
 NUM_TRIALS = 50 # number of tests for each type
 
 # Set resolvers and endpoints
@@ -92,11 +93,16 @@ def run_dns_udp_tests(resolver_name, resolver_ip, logs, avgs_lat, avgs_rb):
     # run warm
     run_dns_udp(resolver_warm, logs, avgs_lat, avgs_rb, resolver_name, run_cold=False)
 
+    resolver_rw = dns.resolver.Resolver()
+    resolver_rw.nameservers = [resolver_ip]
+    # run real world
+    run_dns_udp_rw(resolver_rw, logs, avgs_lat, avgs_rb, resolver_name)
+
 def run_dns_udp(resolver, logs, avgs_lat, avgs_rb, resolver_name, run_cold):
     if run_cold:
-        print("Running cold")
+        print("Running cold...")
     else:
-        print("Running warm")
+        print("Running warm...")
     sum_latency = 0
     sum_rb = 0
     for i in range(NUM_TRIALS):
@@ -137,6 +143,41 @@ def run_dns_udp(resolver, logs, avgs_lat, avgs_rb, resolver_name, run_cold):
     avgs_lat.append(avg_latency)
     avgs_rb.append(avg_rb)
 
+def run_dns_udp_rw(resolver, logs, avgs_lat, avgs_rb, resolver_name):
+    print("Running real world...")
+    sum_lat = 0
+    sum_rb = 0
+    i = 0
+    for query_name in QUERY_LIST:
+        try:
+            start = time.perf_counter()
+            answer = resolver.resolve(query_name, "A")
+            end = time.perf_counter()
+
+            # get measurements
+            latency = (end - start) * 1000 # in miliseconds
+            resp_bytes = len(answer.response.to_wire())
+            success = True
+            notes = ""
+        # note exceptions if they occur
+        except Exception as e:
+            latency = 0
+            resp_bytes = 0
+            success = False
+            notes = str(e)
+        sum_lat += latency
+        sum_rb += resp_bytes
+        if resolver_name == "cloudflare":
+            logs['rw_cloud'].append(latency)
+        else:
+            logs['rw_google'].append(latency)
+        log_row([now(), "UDP", resolver_name, "rw", i, query_name, latency, resp_bytes, success, notes])
+        i += 1
+    avg_lat = sum_lat/NUM_TRIALS
+    avg_rb = sum_rb/NUM_TRIALS
+    avgs_lat.append(avg_lat)
+    avgs_rb.append(avg_rb)
+
 # DNS over HTTPS (DoH)
 def run_doh(resolver_name, logs, avgs_lat, avgs_rb):
     print(f"----- RUNNING DNS OVER HTTPS ({resolver_name}) -----")
@@ -148,15 +189,18 @@ def run_doh(resolver_name, logs, avgs_lat, avgs_rb):
     # run cold
     run_doh_cold(resolver_name, avgs_lat, avgs_rb, url, params, headers, logs)
     # run warm
-    run_doh_warm(resolver_name, avgs_lat, avgs_rb, url, params, headers, logs)
+    run_doh_warm(resolver_name, avgs_lat, avgs_rb, url, headers, logs)
+    # run real world
+    run_doh_realworld(resolver_name, avgs_lat, avgs_rb, url, headers, logs)
 
-def run_doh_warm(resolver_name, avgs_lat, avgs_rb, url, params, headers, logs):
-    print("Running warm")
+def run_doh_warm(resolver_name, avgs_lat, avgs_rb, url, headers, logs):
+    print("Running warm...")
     client = httpx.Client(timeout=5.0)
     sum_lat = 0
     sum_rb = 0
     for i in range(NUM_TRIALS):
         try:
+            params = {"name": QUERY_NAME, "type": "A"}
             # run query
             start = time.perf_counter()
             response = client.get(url, params=params, headers=headers)
@@ -189,8 +233,8 @@ def run_doh_warm(resolver_name, avgs_lat, avgs_rb, url, params, headers, logs):
     avgs_lat.append(avg_lat)
     avgs_rb.append(avg_rb)
 
-def run_doh_cold(resolver_name, avgs_lat, avgs_rb,  url, params, headers, logs):
-    print("Running cold")
+def run_doh_cold(resolver_name, avgs_lat, avgs_rb, url, params, headers, logs):
+    print("Running cold...")
     sum_lat = 0
     sum_rb = 0
     for i in range(NUM_TRIALS):
@@ -220,6 +264,44 @@ def run_doh_cold(resolver_name, avgs_lat, avgs_rb,  url, params, headers, logs):
         else:
             logs['cold_google'].append(latency)
         log_row([now(), "DoH", resolver_name, "cold", i, QUERY_NAME, latency, resp_bytes, success, notes])
+    avg_lat = sum_lat/NUM_TRIALS
+    avg_rb = sum_rb/NUM_TRIALS
+    avgs_lat.append(avg_lat)
+    avgs_rb.append(avg_rb)
+
+def run_doh_realworld(resolver_name, avgs_lat, avgs_rb, url, headers, logs):
+    print("Running real world...")
+    client = httpx.Client(timeout=5.0)
+    sum_lat = 0
+    sum_rb = 0
+    i = 0
+    for query_name in QUERY_LIST:
+        try:
+            params = {"name": query_name, "type": "A"}
+            # run query
+            start = time.perf_counter()
+            response = client.get(url, params=params, headers=headers)
+            end = time.perf_counter()
+
+            # get measurements
+            latency = (end - start) * 1000 # in miliseconds
+            resp_bytes = len(response.content)
+            success = (response.status_code == 200)  # 200 = OK
+            notes = f"status={response.status_code}" # 400 = Bad Request
+        except Exception as e:
+            latency = 0
+            resp_bytes = 0
+            success = False
+            notes = str(e)
+        i += 1
+        sum_lat += latency
+        sum_rb += resp_bytes
+        if resolver_name == "cloudflare":
+            logs['rw_cloud'].append(latency)
+        else:
+            logs['rw_google'].append(latency)
+        log_row([now(), "DoH", resolver_name, "rw", i, query_name, latency, resp_bytes, success, notes])
+    client.close()
     avg_lat = sum_lat/NUM_TRIALS
     avg_rb = sum_rb/NUM_TRIALS
     avgs_lat.append(avg_lat)
@@ -266,15 +348,19 @@ def main():
     udp_logs = {
         'cold_cloud': [],
         'warm_cloud': [],
+        'rw_cloud': [],
         'cold_google': [],
-        'warm_google': []
+        'warm_google': [],
+        'rw_google': []
     }
 
     doh_logs = {
         'cold_cloud': [],
         'warm_cloud': [],
+        'rw_cloud': [],
         'cold_google': [],
-        'warm_google': []
+        'warm_google': [],
+        'rw_google': []
     }
 
     dot_logs = {
@@ -285,11 +371,13 @@ def main():
     # logs for averages
     avgs_lat = []
     avgs_rb = []
-    avg_names = ["DNS Cold Cloudflare", "DNS Warm Cloudflare", "DNS Cold Google", "DNS Warm Google",
-                 "DoH Cold Cloudflare", "DoH Warm Cloudflare", "DoH Cold Google", "DoH Warm Google",
-                 "DoT Cloudflare", "DoT Google"]
+    avg_names = ["DNS Cold Cloud", "DNS Warm Cloud", "DNS RW Cloud",
+                 "DNS Cold Google", "DNS Warm Google", "DNS RW Google",
+                 "DoH Cold Cloud", "DoH Warm Cloud", "DoH RW Cloud",
+                 "DoH Cold Google", "DoH Warm Google", "DoH RW Google",
+                 "DoT Cloud", "DoT Google"]
     combo_avgs_lat = []
-    combo_avg_names = ["DNS Cold", "DNS Warm", "DoH Cold", "DoH Warm", "DoT"]
+    combo_avg_names = ["DNS Cold", "DNS Warm", "DNS RW", "DoH Cold", "DoH Warm", "DoH RW", "DoT"]
     combo_avgs_rb = []
     combo_avg_names_rb = ["DNS", "DoH"]
 
@@ -311,22 +399,26 @@ def main():
     print(f"Done running. Results saved to {CSV_FILE}. Plotting graphs...")
 
     # combine the averages of tests with negligible differences
-    combo_avgs_lat.append((avgs_lat[0] + avgs_lat[2])/2) # DNS Cold
-    combo_avgs_lat.append((avgs_lat[1] + avgs_lat[3])/2) # DNS Warm
-    combo_avgs_lat.append((avgs_lat[4] + avgs_lat[6])/2) # DoH Cold
-    combo_avgs_lat.append((avgs_lat[5] + avgs_lat[7])/2) # DoH Warm
-    combo_avgs_lat.append((avgs_lat[8] + avgs_lat[9])/2) # DoT
+    combo_avgs_lat.append((avgs_lat[0] + avgs_lat[3])/2) # DNS Cold
+    combo_avgs_lat.append((avgs_lat[1] + avgs_lat[4])/2) # DNS Warm
+    combo_avgs_lat.append((avgs_lat[2] + avgs_lat[5])/2) # DNS Real World
+    combo_avgs_lat.append((avgs_lat[6] + avgs_lat[9])/2) # DoH Cold
+    combo_avgs_lat.append((avgs_lat[7] + avgs_lat[10])/2) # DoH Warm
+    combo_avgs_lat.append((avgs_lat[8] + avgs_lat[11])/2) # DoH Real World
+    combo_avgs_lat.append((avgs_lat[12] + avgs_lat[13])/2) # DoT
 
-    combo_avgs_rb.append((avgs_rb[0] + avgs_rb[1] + avgs_rb[2] + avgs_rb[3])/4) # All DNS
-    combo_avgs_rb.append((avgs_rb[4] + avgs_rb[5] + avgs_rb[6] + avgs_rb[7])/4) # All DoH
+    combo_avgs_rb.append((avgs_rb[0] + avgs_rb[1] + avgs_rb[2] + avgs_rb[3] + avgs_rb[4] + avgs_rb[5])/6) # All DNS
+    combo_avgs_rb.append((avgs_rb[6] + avgs_rb[7] + avgs_rb[8] + avgs_rb[9] + avgs_rb[10] + avgs_rb[11])/6) # All DoH
 
     # print first values and averages
     print("----- Latency Averages -----")
-    print(f"DNS Cold Average: {combo_avgs_lat[0]:.2f} ms")
-    print(f"DNS Warm Average: {combo_avgs_lat[1]:.2f} ms")
-    print(f"DoH Cold Average: {combo_avgs_lat[2]:.2f} ms")
-    print(f"DoH Warm Average: {combo_avgs_lat[3]:.2f} ms")
-    print(f"DoT Average:      {combo_avgs_lat[4]:.2f} ms")
+    print(f"DNS Cold Average:         {combo_avgs_lat[0]:.2f} ms")
+    print(f"DNS Warm Average:         {combo_avgs_lat[1]:.2f} ms")
+    print(f"DNS \"Real World\" Average: {combo_avgs_lat[2]:.2f} ms")
+    print(f"DoH Cold Average:         {combo_avgs_lat[3]:.2f} ms")
+    print(f"DoH Warm Average:         {combo_avgs_lat[4]:.2f} ms")
+    print(f"DoH \"Real World\" Average: {combo_avgs_lat[5]:.2f} ms")
+    print(f"DoT Average:              {combo_avgs_lat[6]:.2f} ms")
     print("----- Average Number of Response Bytes -----")
     print(f"DNS: {combo_avgs_rb[0]:.1f} bytes")
     print(f"DoH: {combo_avgs_rb[1]:.1f} bytes")
@@ -340,12 +432,15 @@ def main():
     fig6 = plt.figure(figsize=(8,6))
     fig7 = plt.figure(figsize=(8,6))
     fig8 = plt.figure(figsize=(8,6))
+    fig9 = plt.figure(figsize=(8,6))
 
     udp_plot = fig1.add_subplot(111)
     udp_plot.plot(trials, udp_logs["cold_cloud"], label='Cold with Cloudflare')
     udp_plot.plot(trials, udp_logs["cold_google"], label='Cold with Google')
     udp_plot.plot(trials, udp_logs["warm_cloud"], label='Warm with Cloudflare')
     udp_plot.plot(trials, udp_logs["warm_google"], label='Warm with Google')
+    udp_plot.plot(trials, udp_logs["rw_cloud"], label='RW with Cloudflare')
+    udp_plot.plot(trials, udp_logs["rw_google"], label='RW with Google')
     udp_plot.set_xlabel("Trial Number")
     udp_plot.set_ylabel("Latency (ms)")
     udp_plot.set_title("Standard DNS")
@@ -356,6 +451,8 @@ def main():
     doh_plot.plot(trials, doh_logs["cold_google"], label='Cold with Google')
     doh_plot.plot(trials, doh_logs["warm_cloud"], label='Warm with Cloudflare')
     doh_plot.plot(trials, doh_logs["warm_google"], label='Warm with Google')
+    doh_plot.plot(trials, doh_logs["rw_cloud"], label='RW with Cloudflare')
+    doh_plot.plot(trials, doh_logs["rw_google"], label='RW with Google')
     doh_plot.set_xlabel("Trial Number")
     doh_plot.set_ylabel("Latency (ms)")
     doh_plot.set_title("DNS over HTTPS (DoH)")
@@ -369,41 +466,54 @@ def main():
     dot_plot.set_title("DNS over TLS (DoT)")
     dot_plot.legend()
 
-    all_three = fig4.add_subplot(111)
-    all_three.plot(trials, udp_logs["cold_cloud"], label='DNS Cold with Cloudflare')
-    all_three.plot(trials, udp_logs["cold_google"], label='DNS Cold with Google')
-    all_three.plot(trials, udp_logs["warm_cloud"], label='DNS Warm with Cloudflare')
-    all_three.plot(trials, udp_logs["warm_google"], label='DNS Warm with Google')
-    all_three.plot(trials, doh_logs["cold_cloud"], label='DOH Cold with Cloudflare')
-    all_three.plot(trials, doh_logs["cold_google"], label='DOH Cold with Google')
-    all_three.plot(trials, doh_logs["warm_cloud"], label='DOH Warm with Cloudflare')
-    all_three.plot(trials, doh_logs["warm_google"], label='DOH Warm with Google')
-    all_three.plot(trials, dot_logs["cloud"], label='DoT with Cloudflare')
-    all_three.plot(trials, dot_logs["google"], label='DoT with Google')
-    all_three.set_xlabel("Trial Number")
-    all_three.set_ylabel("Latency (ms)")
-    all_three.set_title("Standard DNS v. DNS over HTTPS (DoH)")
-    all_three.legend()
+    all_three_c = fig4.add_subplot(111)
+    all_three_c.plot(trials, udp_logs["cold_cloud"], label='DNS Cold with Cloudflare')
+    all_three_c.plot(trials, udp_logs["warm_cloud"], label='DNS Warm with Cloudflare')
+    all_three_c.plot(trials, udp_logs["rw_cloud"], label='DNS RW with Cloudflare')
+    all_three_c.plot(trials, doh_logs["cold_cloud"], label='DoH Cold with Cloudflare')
+    all_three_c.plot(trials, doh_logs["warm_cloud"], label='DoH Warm with Cloudflare')
+    all_three_c.plot(trials, doh_logs["rw_cloud"], label='DoH RW with Cloudflare')
+    all_three_c.plot(trials, dot_logs["cloud"], label='DoT with Cloudflare')
+    all_three_c.set_xlabel("Trial Number")
+    all_three_c.set_ylabel("Latency (ms)")
+    all_three_c.set_title("Standard DNS v. DNS over HTTPS (DoH) v. DNS over TLS (DoT)")
+    all_three_c.legend()
 
-    avgs_lat_bar = fig5.add_subplot(111)
-    avgs_lat_bar.bar(avg_names, avgs_lat)
+    all_three_g = fig5.add_subplot(111)
+    all_three_g.plot(trials, udp_logs["cold_google"], label='DNS Cold with Google')
+    all_three_g.plot(trials, udp_logs["warm_google"], label='DNS Warm with Google')
+    all_three_g.plot(trials, udp_logs["rw_google"], label='DNS RW with Google')
+    all_three_g.plot(trials, doh_logs["cold_google"], label='DoH Cold with Google')
+    all_three_g.plot(trials, doh_logs["warm_google"], label='DoH Warm with Google')
+    all_three_g.plot(trials, doh_logs["rw_google"], label='DoH RW with Google')
+    all_three_g.plot(trials, dot_logs["google"], label='DoT with Google')
+    all_three_g.set_xlabel("Trial Number")
+    all_three_g.set_ylabel("Latency (ms)")
+    all_three_g.set_title("Standard DNS v. DNS over HTTPS (DoH) v. DNS over TLS (DoT)")
+    all_three_g.legend()
+
+    avgs_lat_bar = fig6.add_subplot(111)
+    bars1 = avgs_lat_bar.bar(avg_names, avgs_lat)
+    avgs_lat_bar.bar_label(bars1, fmt="%.2f ms")
     avgs_lat_bar.set_ylabel("Latency (ms)")
     avgs_lat_bar.set_title(f"Average Latencies(ms) for {NUM_TRIALS} Trials")
 
-    combo_avgs_lat_bar = fig6.add_subplot(111)
-    combo_avgs_lat_bar.bar(combo_avg_names, combo_avgs_lat)
+    combo_avgs_lat_bar = fig7.add_subplot(111)
+    bars2 = combo_avgs_lat_bar.bar(combo_avg_names, combo_avgs_lat)
+    combo_avgs_lat_bar.bar_label(bars2, fmt="%.2f ms")
     combo_avgs_lat_bar.set_ylabel("Latency (ms)")
     combo_avgs_lat_bar.set_title(f"Average Latencies(ms) for {NUM_TRIALS*2} Trials")
 
-    avgs_rb_bar = fig7.add_subplot(111)
-    avgs_rb_bar.bar(avg_names[:8], avgs_rb)
+    avgs_rb_bar = fig8.add_subplot(111)
+    bars3 = avgs_rb_bar.bar(avg_names[:12], avgs_rb)
+    avgs_rb_bar.bar_label(bars3, fmt="%.2f ms")
     avgs_rb_bar.set_ylabel("Number of Bytes in Response")
     avgs_rb_bar.set_title(f"Average Number of Response Bytes for {NUM_TRIALS} Trials")
 
-    combo_avgs_rb_bar = fig8.add_subplot(111)
+    combo_avgs_rb_bar = fig9.add_subplot(111)
     combo_avgs_rb_bar.bar(combo_avg_names_rb, combo_avgs_rb)
     combo_avgs_rb_bar.set_ylabel("Number of Bytes in Response")
-    combo_avgs_rb_bar.set_title(f"Average Number of Response Bytes for {NUM_TRIALS*4} Trials")
+    combo_avgs_rb_bar.set_title(f"Average Number of Response Bytes for {NUM_TRIALS*6} Trials")
 
     plt.show()
 
